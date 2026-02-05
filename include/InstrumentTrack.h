c844d91cd494bc174a81badd1615bd3c15a0701a
jlintgod <jlintgod@gmail.com>
2026-02-05 13:48:46 -0700

    Add crudely coded humanization controls

--- include/InstrumentTrack.h
+++ include/InstrumentTrack.h
@@ -242,6 +242,16 @@ public:
 		return &m_useMasterPitchModel;
 	}
 
+	FloatModel* randomPitchMinModel()
+	{
+		return &m_randomPitchMinModel;
+	}
+
+	FloatModel* randomPitchMaxModel()
+	{
+		return &m_randomPitchMaxModel;
+	}
+
 	void setPreviewMode( const bool );
 
 	bool isPreviewMode() const
@@ -317,6 +327,8 @@ private:
 	IntModel m_pitchRangeModel;
 	IntModel m_mixerChannelModel;
 	BoolModel m_useMasterPitchModel;
+	FloatModel m_randomPitchMinModel;
+	FloatModel m_randomPitchMaxModel;
 
 	Instrument * m_instrument;
 	InstrumentSoundShaping m_soundShaping;
