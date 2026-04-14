a7616e54659587215223ab2bb0edce2202787fe9
jlintgod1 <jlintgod@gmail.com>
2026-04-14 17:54:00 -0600

    Make .dls valid in DataFile

--- src/core/DataFile.cpp
+++ src/core/DataFile.cpp
@@ -229,7 +229,7 @@ bool DataFile::validate( QString extension )
 		if (! ( extension == "mmp" || extension == "mpt" || extension == "mmpz" ||
 				extension == "xpf" || extension == "xml" ||
 				( extension == "xiz" && ! getPluginFactory()->pluginSupportingExtension(extension).isNull()) ||
-				extension == "sf2" || extension == "sf3" || extension == "pat" || extension == "mid" ||
+				extension == "sf2" || extension == "sf3" || extension == "dls" || extension == "pat" || extension == "mid" ||
 				extension == "dll"
 #ifdef LMMS_BUILD_LINUX
 				|| extension == "so"
